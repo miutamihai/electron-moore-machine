2b2024e40125d3e88ada111833d2a4baf2fb91e3
miutamihai <miuta.mihai@gmail.com>
2022-11-07 18:51:35 +0200

    [main] Add overflow to state

--- src/state/index.jsx
+++ src/state/index.jsx
@@ -4,10 +4,12 @@ import { Change } from './change'
 import { makeRandomString } from '../makeRandomString'
 import { RenderIf } from '../renderIf'
 
+const scrollBarClasses = 'scrollbar-thin scrollbar-thumb-gray-600 scrollbar-track-gray-800 overflow-y-scroll scrollbar-thumb-rounded-full scrollbar-track-rounded-full hover:scrollbar-thumb-slate-50'
+
 export const State = () => {
     const [ state, setState ] = useState([])
 
-    return <div className={ 'flex flex-col space-y-2 w-3/4' }>
+    return <div className={ `flex flex-col space-y-2 p-4 w-3/4 overflow-auto ${scrollBarClasses}` }>
         { state.map(value => <Pair key={ value } value={ value }/>) }
         <Change onClick={ () => setState(prev => [ ...prev, makeRandomString() ]) }>
             +
