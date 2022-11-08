47a5f30966eb4abad2d7af6c3b5a8aaafea6e6f2
miutamihai <miuta.mihai@gmail.com>
2022-11-08 02:52:23 +0200

    [main] Clear file inputs

--- src/setup/submit/useGoToDisplay.js
+++ src/setup/submit/useGoToDisplay.js
@@ -10,7 +10,7 @@ export const useGoToDisplay = () => {
         if (input) {
             dataSet = input.split(',')
         } else {
-            dataSet = data.split(',')
+            dataSet = data.trim().replaceAll(' ', '').split(',')
         }
         setDataSet(dataSet)
         setStage(stages.display)
